8d04770aeba4d1ea02ec1c7451c29c3011d2ff68
papitamcfly <papitamcfly1234@gmail.com>
2024-02-28 21:51:05 -0600

    scrr

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  keyframes,
+  // ...
+} from '@angular/animations';
 import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports:[RouterOutlet,CommonModule ],
+  standalone: true,
+  animations:[
+    trigger('openClose',[
+    state('open', style({
+      height: '200px',
+      opacity: 1,
+      backgroundColor: 'yellow'
+    })),
+    state('closed', style({
+      height: '100px',
+      opacity: 0.8,
+      backgroundColor: 'blue'
+    })),
+    transition('open => closed', [
+      animate('1s')
+    ]),
+    transition('closed => open', [
+      animate('0.5s')
+    ]),
+  ]),
+  trigger('myInsertRemoveTrigger', [
+    transition(':enter', [
+      style({ opacity: 0 }),
+      animate('100ms', style({ opacity: 1 })),
+    ]),
+    transition(':leave', [
+      animate('100ms', style({ opacity: 0 }))
+    ])
+  ]),
+  ]
+ 
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'animations';
+  isOpen = true;
+  showElement= false;
+  isShown = false;
+  toggle() {
+    this.isOpen = !this.isOpen;
+  }
 }
---
+++ src/app/open-close/open-close.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  // ...
+} from '@angular/animations';
+
+@Component({
+  selector: 'app-open-close',
+  standalone: true,
+  imports: [],
+  templateUrl: './open-close.component.html',
+  styleUrl: './open-close.component.css'
+})
+export class OpenCloseComponent {
+
+}
