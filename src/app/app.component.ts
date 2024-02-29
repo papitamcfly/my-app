1226a95553116b0484ba5d8025ff1e5c3eeae367
papitamcfly <papitamcfly1234@gmail.com>
2024-02-29 00:11:43 -0600

    asd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { ChildrenOutletContexts } from '@angular/router';
 import { slideInAnimation } from './animations';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { trigger, state, style, transition, animate } from '@angular/animations';
+import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
 
 @Component({
   standalone: true,
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css'],
-  imports: [RouterLink, RouterOutlet],
+  imports: [RouterLink, RouterOutlet,CommonModule],
   animations:[
     trigger('openClose', [
       // ...
